ad505625e1c1ee546e5b51114e6402a6fac44d6d
Borisevich N V <borisevich170@yandex.ru>
2021-09-14 21:40:46 +0300

    Base code added

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main()
+{
+
+
+
+    std::cout << "Enter your name and surname, please: ";
+    std::string sName;
+    std::string sSurname;
+
+    std::cin >> sName >> sSurname;
+
+    std::cout << std::endl << "Enter your favorite film, please: ";
+    std::string sFilm;
+
+    std::cin >> sFilm;
+
+    std::ofstream fout("biglist.txt");
+
+    fout << sName << " " << sSurname << " - " << sFilm << ";" << std::endl;
+    fout.close();
+
+
+    return 0;
+}
