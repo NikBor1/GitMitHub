5a79969f671d2bc254c166e4229253911cd18a13
Borisevich N V <borisevich170@yandex.ru>
2021-09-16 00:44:32 +0300

    dded question list

--- main.cpp
+++ main.cpp
@@ -6,21 +6,20 @@ int main()
 {
 
 
-
-    std::cout << "Enter your name and surname, please: ";
-    std::string sName;
-    std::string sSurname;
-
-    std::cin >> sName >> sSurname;
-
-    std::cout << std::endl << "Enter book, you recommend to read, please: ";
-    std::string sFilm;
-
-    std::cin >> sFilm;
-
+    std::ifstream fin("questionlist.txt");
     std::ofstream fout("biglist.txt");
 
-    fout << sName << " " << sSurname << " - " << sFilm << ";" << std::endl;
+    std::string question;
+    std::string answer;
+
+    while(getline(fin, question))
+    {
+        std::cout << question << " ";
+        getline(std::cin, answer);
+        fout << answer << std::endl;
+    }
+
+    fin.close();
     fout.close();
 
 
