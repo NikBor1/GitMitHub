ecbf831be9ed0fc31175145cbe6c943d7195958b
Borisevich N V <borisevich170@yandex.ru>
2021-09-21 01:08:21 +0300

    Added sorted answer output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 int main()
 {
@@ -11,7 +12,7 @@ int main()
 
     if(choiser == 1)
     {
-         std::ifstream fin("questionlist.txt");
+        std::ifstream fin("questionlist.txt");
         std::ofstream fout("biglist.txt");
 
         std::string question;
@@ -32,14 +33,43 @@ int main()
     else
     {
         std::string answer;
+        std::vector <std::vector <std::string> > answerTable (3);
 
         std::ifstream fin("biglist.txt");
 
-        while(getline(fin, answer))
-            std::cout << answer << std::endl;
+        int ii = 0;
 
+        while(fin >> answer)
+        {
+            answerTable[ii % 3].push_back(answer);
+            ii ++;
+        }
+
+        ii /= 3;
 
         fin.close();
+
+
+        for(int i = 0; i < ii; i ++)
+        {
+            for(int j = 1; j < ii - i; j ++)
+            {
+
+                if(answerTable[1][j] < answerTable[1][j - 1])
+                {
+                    std::swap(answerTable[1][j], answerTable[1][j - 1]);
+                    std::swap(answerTable[0][j], answerTable[0][j - 1]);
+                    std::swap(answerTable[2][j], answerTable[2][j - 1]);
+                }
+
+
+            }
+        }
+
+        for(int i = 0; i < ii; i ++)
+            std::cout << answerTable[1][i] << " " << answerTable[0][i] << " " << answerTable[2][i] << std::endl;
+
+
     }
 
     return 0;
